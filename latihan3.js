664c695e15d493e620194809ff333ddf59929975
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-07 14:19:49 +0700

    add latihan3

--- latihan3.js
+++ latihan3.js
@@ -1,8 +1,25 @@
 var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
+function dataHandling2(input) {
+    var date = '';
+    input.splice(1, 2, 'Roman Alamsyah Elsharawy', 'Provinsi Bandar Lampung');
+    input.splice(4, 1, 'Pria', 'SMA Internasional Metro');
+    date = input[3].split('/');
+    var dateSort = input[3].split('/');
+    dateSort = dateSort.sort(function (a, b) {
+        return Number(b) > Number(a);
+    })
+    var mm = date[1];
 
-// function dataHandling2 (input) {
-//     input.splice(1,0,'Elsharawy');
-// }
+    var dateJoin = date.join('-');
+    console.log(input);
+    switch (mm) {
+        case '05':
+            console.log('Mei');
+            break;
+    }
+    console.log(dateSort);
+    console.log(dateJoin);
+    console.log(input[1].slice(0,15));
 
-var splice = input[1].splice(1,0,'Elsharawy');
-console.log(splice);
+}
+dataHandling2(input)
