f043ae3c756489130e891b9f03cd020eb2e2d46e
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-10 19:27:59 +0700

    add ex 25

---
+++ latihan25.js
@@ -0,0 +1,37 @@
+/*
+==================================
+Array Mastery: Group Odd Evens
+==================================
+
+Nama:SEPTIAN MAULANA
+
+[INSTRUKSI]
+Function groupOddEven akan menerima satu parameter berupa array yang berisi number.
+Function akan mengelompokkan setiap number yang ganjil (odd) dan setiap number yang genap (even),
+dan akan mereturn sebuah string dengan format berikut:
+
+"ODDS: <OddNum1>,<OddNum2>,<OddNum3> EVENS: <EvenNum1>,<EvenNum2>,<EvenNum3>"
+
+aturan:
+  - Apabila tidak ada angka ganjil, hanya tampilkan:
+  "EVENS: <EvenNum1>, <EvenNum2>"
+  - Apabila tidak ada angka genap, hanya tampilkan:
+  "ODDS: <OddNum1>, <OddNum2>"
+  - Apabila tidak ada angka sama sekali, tampilkan string kosong!
+  - Angka tidak perlu diurutkan!
+
+[CONTOH]
+input: [1, 5, 8, 2, 3]
+output: "ODDS: 1, 5, 3 EVENS: 8, 2"
+
+input: [1, 1, 1]
+output: "ODDS: 1, 1, 1"
+
+input: [2, 8, 10]
+output: "EVENS: 2, 8, 10"
+*/
+
+/*
+Simpan variabel students dalam bentuk array tipe data angka
+Return angka ganjil masuk ke ODDS dan angka genap masuk ke EVENS
+*/
