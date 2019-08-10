68d848e0159ddc7fc1654dbe7eca28cf9173fb00
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-10 19:07:40 +0700

    fixed ex 23

--- latihan23.js
+++ latihan23.js
@@ -10,7 +10,8 @@ Setiap student di HACKTIV8 ditugaskan untuk membuat tim project dengan komposisi
 Kita membutuhkan sebuah program untuk membuat tim-tim tersebut.
 
 Function pairUp akan menerima satu parameter berupa array yang berisi string nama student.
-Function ini akan mengelompokkan setiap dua nama menjadi satu kelompok, dan apabila tersisa satu student, student terakhir akan bersama instruktur.
+Function ini akan mengelompokkan setiap dua nama menjadi satu kelompok, dan apabila tersisa satu student, 
+student terakhir akan bersama instruktur.
 Hasil pengelompokkan adalah sebuah array yang berisi string berupa nama dua orang student sebagai satu tim.
 Formatnya adalah <Nama Student 1> dan <Nama Student 2>.
 [CONTOH]
@@ -30,3 +31,19 @@ Simpan variabel Students berupa array dengan tipe data string
 Return 2 nama dalam bentuk gabungan array dengan kata hubung 'dan'
 Jika tidak habis dibagi 2 maka sisanya students berpasangan dengan Instruktur
 */
+
+function pairUp (students) {
+    var pairUpresult = [];
+    if (students.length % 2 != 0) {
+        students.push('Instruktur');
+    }
+    for (var i = 0; i < students.length; i++) {
+        pairUpresult.push(students[i] + ' dan ' + students[i+1]);
+        i++;
+    }
+    return pairUpresult;
+}
+console.log(pairUp(['Budi', 'Badu', 'Indra', 'Indri']));
+console.log(pairUp(['Budi', 'Badu', 'Indra', 'Indri', 'james']));
+console.log(pairUp([]));
+console.log(pairUp(['arnold','risan','devita','elisabeth','bram','roby','evans']));
