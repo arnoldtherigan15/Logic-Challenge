d57d94040e53bdb4a450fefed3440096dd032211
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-10 22:13:30 +0700

    fixed 27

--- latihan27.js
+++ latihan27.js
@@ -1,2 +1,14 @@
 var num = [5, 2, 1, 4];
 var tampung = 0;
+for (var i = 0; i < num.length; i++) {
+    for (var j = 0; j < num.length; j++) {
+        if (num[j] < num[j+1]) {
+            tampung = num[j];
+            num[j] = num[j+1];
+            num[j+1] = tampung;
+        }
+    }
+    
+}
+console.log(num);
+
