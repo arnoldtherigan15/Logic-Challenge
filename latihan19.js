41a6381eedd43f99b145d283045295711c1298e8
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-10 15:38:18 +0700

    fixed 19

--- latihan19.js
+++ latihan19.js
@@ -1,1 +1,9 @@
-var angka = [3,1,6,19,8,50,29];
+var angka = [1,5,2,30,17,18];
+for (var i = 0; i < angka.length; i++) {
+    if (angka[i] > angka [i+1]) {
+        var tampung = angka[i];
+        angka[i] = angka[i+1];
+        angka[i+1] = tampung;
+    }
+}
+console.log(angka);
