f1489a86cda18395c0bee1a21b392dbde3ce264b
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-10 14:09:15 +0700

    fixed ex 18

--- latihan18.js
+++ latihan18.js
@@ -1,10 +1,17 @@
 function pasanganTerbesar(num) {
-    // you can only write your code here!
-  }
+    var string = String(num);
+    var array = string.split('');
+    var arrayBaru = [];
+    for (var i = 0; i < array.length - 1; i++) {
+        arrayBaru.push(Number(array[i] + array[i + 1]));
+    }
+    arrayBaru.sort();
+    return arrayBaru[arrayBaru.length - 1];
+}
   
-  // TEST CASES
-  console.log(pasanganTerbesar(641573)); // 73
-  console.log(pasanganTerbesar(12783456)); // 83
-  console.log(pasanganTerbesar(910233)); // 91
-  console.log(pasanganTerbesar(71856421)); // 85
-  console.log(pasanganTerbesar(79918293)); // 99
+// TEST CASES
+console.log(pasanganTerbesar(641573)); // 73
+console.log(pasanganTerbesar(12783456)); // 83
+console.log(pasanganTerbesar(910233)); // 91
+console.log(pasanganTerbesar(71856421)); // 85
+console.log(pasanganTerbesar(79918293)); // 99
