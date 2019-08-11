0291baf0fa918a96c10cc7077daee6df72cc8259
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-11 17:39:08 +0700

    ex38

---
+++ latihan38.js
@@ -0,0 +1,28 @@
+/*
+HACKTIV8 Live code week 4
+
+Nama: andrawina kirana adi
+
+===============================
+Soal Array 3 - Remove Duplicate
+===============================
+
+[INSTRUKSI]
+Tersedia satu input array yang memiliki angka yang duplikat, Tugas kamu menghapus angka yang duplikat tersebut.
+
+Output: array yang telah di-sorted, semua duplikat dihapus
+
+[CONTOH]
+
+1) [1, 4, 3, 1, 2, 2, 4]	-->	[1, 2, 3, 4]
+
+*/
+
+function hapusDuplikat(arr) {
+    // start code here
+}
+
+console.log(hapusDuplikat([1, 4, 3, 1, 2, 2, 4])); // [1, 2, 3, 4]
+console.log(hapusDuplikat([2, 2, 2, 2])); // [2]
+console.log(hapusDuplikat([1, 2, 3])); // [1, 2, 3]
+console.log(hapusDuplikat([6, 5, 3, 2])); // [2, 3, 5, 6]
