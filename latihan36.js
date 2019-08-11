56138c86f20918fe3491cc1b17f54ada29ac2451
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-11 17:36:18 +0700

    add ex 36

---
+++ latihan36.js
@@ -0,0 +1,27 @@
+/*
+HACKTIV8 Live code week 4
+
+Nama: andrawina kirana adi
+
+=======================
+Soal Array 1 - Pair Sum
+=======================
+
+[INSTRUKSI]
+Diberikan 2 input: array dan targetSum
+Output ada berapa pasangan nilai dalam array yang memiliki jumlah sama dengan
+targetSum, sepasang nilai tidak boleh memiliki indeks yang sama
+
+[CONTOH]
+1)	[1 ,2, 3, 4, 5], targetSum = 8		-->		1
+*/
+
+function pairSum(arr, targetSum){
+    // start code here
+
+  }
+  
+console.log(pairSum([1, 2, 3, 4, 5], 8));	// 1
+console.log(pairSum([3, 2, 1, 5, 2], 4));	// 2
+console.log(pairSum([1, 1, 1], 2)); // 3
+console.log(pairSum([1, 2, 3], 6)); // 0
