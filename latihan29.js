979c671e484c82dee2a041203e2a385dbafd3be0
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-11 17:16:27 +0700

    add latihan29

---
+++ latihan29.js
@@ -0,0 +1,15 @@
+// ==================================
+// Array Mastery: Highest Number
+// ==================================
+// Nama:________
+// [INSTRUKSI]
+// Function highestNumber akan menerima satu parameter berupa array yang berisikan angka, dimana dengan asumsi kemungkinan angka dari 0 - 999, dan angka dalam array minimal 3 angka, dan tidak ada angka yang bernilai sama (ini adalah info tese case, bukan harus divalidasi/dicek).
+// highestNumber akan mengembalikan angka yang terbesar dari array tersebut.
+// [CONTOH]
+// input: [4, 2, 5, 1]
+// output: 5
+// [RULE]
+// - Dilarang menggunakan Regex (.match, .test, dan sebagainya)
+// - Dilarang menggunakan method .filter, atau .reduce!
+// - Dilarang menggunakan .sort!
+// - Dilarang menggunakan built in object (Math.min, Math.max, dsb)
