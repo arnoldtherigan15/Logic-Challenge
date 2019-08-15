40068c9fef8b694c69b705db844f0c1b8ca980c1
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-15 13:44:26 +0700

    fixed latihan40

--- latihan40.js
+++ latihan40.js
@@ -1,9 +1,25 @@
 function changeMe(arr) {
-    // you can only write your code here!
+    var currentYear = 2019;
+    for (var i = 0; i < arr.length; i++) {
+        console.log((i+1) + '. ' + arr[i][0] + ' ' + arr[i][1] + ':');
+        var object = {};
+        object.firstName = arr[i][0];
+        object.lastName = arr[i][1];
+        object.gender = arr[i][2];
+        if (arr[i][3] === undefined || arr[i][3] > currentYear) {
+            object.age = 'Invalid Birth Year';
+            console.log(object);
+            break;
+        }
+        object.age = currentYear - arr[i][3];
+        console.log(object);
+    }
+   
 }
 
 // TEST CASES
-changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); // 1. Christ Evans:
+changeMe([['Christ', 'Evans', 'Male', 1982], ['Robert', 'Downey', 'Male']]); 
+// 1. Christ Evans:
 // { firstName: 'Christ',
 //   lastName: 'Evans',
 //   gender: 'Male',
