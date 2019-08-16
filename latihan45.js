65953c0f8bff5be2a1cc12b1da20f38307d92ba8
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-16 14:06:05 +0700

    fixed ex 45

--- latihan45.js
+++ latihan45.js
@@ -1,6 +1,27 @@
 function naikAngkot(arrPenumpang) {
     rute = ['A', 'B', 'C', 'D', 'E', 'F'];
-    //your code here
+    var indesk1 = 0;
+    var indesk2 = 0;
+    var result = [];
+    var total = 0;
+    for (var i = 0; i < arrPenumpang.length; i++) {
+        var object = {};
+        for (var j = 0; j < rute.length; j++) {
+            if (arrPenumpang[i][1] === rute[j]) {
+                indesk1 = j+1;
+            } else if (arrPenumpang[i][2] === rute[j]) {
+                indesk2 = j+1;
+            }
+            total = (indesk2 - indesk1)*2000;
+        }
+    
+        object.penumpang = arrPenumpang[i][0];
+        object.naikDari = arrPenumpang[i][1];
+        object.tujuan = arrPenumpang[i][2];
+        object.bayar = total;
+        result.push(object);
+    }
+    return result;
 }
 
 //TEST CASE
