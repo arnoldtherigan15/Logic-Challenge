32e8c1d61d77d83857e9f756225420065fcc43c8
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-16 11:07:12 +0700

    add ex 43

--- latihan42.js
+++ latihan42.js
@@ -1,10 +1,39 @@
+
 function countProfit(shoppers) {
     let listBarang = [ ['Sepatu Stacattu', 1500000, 10],
                        ['Baju Zoro', 500000, 2],
                        ['Sweater Uniklooh', 175000, 1]
                      ];
-  
-    // you can only write your code here!
+    var data = [];
+    if (shoppers.length === 0) {
+        return data;
+    }
+    for (var i = 0; i < listBarang.length; i++) {
+        var object = {};
+        var nama = [];
+        var sisa = listBarang[i][2];
+        var profit = listBarang[i][1];
+        var totalAmount = 0;
+        for (var j = 0; j < shoppers.length; j++) {
+            if (listBarang[i][0] === shoppers[j].product && sisa >= shoppers[j].amount) {
+                nama.push(shoppers[j].name);
+                object.product = listBarang[i][0];
+                sisa -= shoppers[j].amount;
+                totalAmount += shoppers[j].amount;
+
+            } else {
+                object.product = listBarang[i][0];
+                object.shoppers = [];
+                object.leftOver = listBarang[i][2];
+                object.totalProfit = 0;
+            }
+        }
+        object.shoppers = nama;
+        object.leftOver = sisa;
+        object.totalProfit = totalAmount * profit;
+        data.push(object);
+    }
+    return data;
 }
 
 // TEST CASES
@@ -35,7 +64,7 @@ console.log(countProfit([{name: 'Windi', product: 'Sepatu Stacattu', amount: 8},
 //     shoppers: [ 'Rani' ],
 //     leftOver: 0,
 //     totalProfit: 175000 } ]
-console.log(countProfit([{name: 'Windi', product: 'Sepatu Naiki', amount: 5}]));
+// console.log(countProfit([{name: 'Windi', product: 'Sepatu Naiki', amount: 5}]));
 // [ { product: 'Sepatu Stacattu',
 //     shoppers: [],
 //     leftOver: 10,
@@ -48,4 +77,4 @@ console.log(countProfit([{name: 'Windi', product: 'Sepatu Naiki', amount: 5}]));
 //     shoppers: [],
 //     leftOver: 1,
 //     totalProfit: 0 } ]
-console.log(countProfit([])); //[]
+// console.log(countProfit([])); //[]
