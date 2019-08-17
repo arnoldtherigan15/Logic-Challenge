8e6b0ba9aebadc920abc8201ed671e3a287ab05b
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-17 11:57:55 +0700

    add ex 56

---
+++ latihan56.js
@@ -0,0 +1,55 @@
+/**
+CSV to Object
+==============
+Implementasikan function `csvToObject` untuk merubah string yang
+diberikan menjadi sebuah object baru.
+`csvToObject` menerima 2 parameter yaitu `keys` dan `values`
+yang merupakan sebuah CSV (Comma Separated Values)
+Keys & values di dalam object baru nantinya akan diambil dari kedua
+parameter yang diberikan.
+Contoh:
+- Input: 'name,age', 'Aang,112'
+  Output: { name: 'Aang', age: '112' }
+- Input: 'title,description,content', 'Foobar,Foo and Bar,Foobar content'
+  Output: { title: 'Foobar', description: 'Foo and Bar', content: 'Foobar Content' }
+- Input: '', ''
+  Output: { }
+*/
+
+// function csvToObject (keys, values) {
+//   var keysArr = keys.split(',');
+//   var valuesArr = values.split(',');
+//   var obj = {};
+//   if (keys.length === 0 && values.length === 0) {
+//     return obj;
+//   }
+//   else {
+//     for (var i=0; i<keysArr.length; i++) {
+//       obj[keysArr[i]] = valuesArr[i];
+//     }
+//     return obj;
+//   }
+
+// }
+
+// console.log(csvToObject('name,phoneNumber', 'Dimitri,+666123654'));
+// // { name: 'Dimitri', phoneNumber: '+666123654' }
+
+// console.log(csvToObject('firstName,lastName,nationality', 'Sergei,Dragunov,Russia'));
+// // { firstName: 'Sergei', lastName: 'Dragunov', nationality: 'Russia' }
+
+// console.log(csvToObject('', ''));
+// // { }
+
+function csvToObject (keys, values) {
+
+}
+
+console.log(csvToObject('name,phoneNumber', 'Dimitri,+666123654'));
+// { name: 'Dimitri', phoneNumber: '+666123654' }
+
+console.log(csvToObject('firstName,lastName,nationality', 'Sergei,Dragunov,Russia'));
+// { firstName: 'Sergei', lastName: 'Dragunov', nationality: 'Russia' }
+
+console.log(csvToObject('', ''));
+// { }
