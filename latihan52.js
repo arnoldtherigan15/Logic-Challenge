c02fdf138f486ddd851a434e190cb914ebee8f18
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-17 09:17:23 +0700

    fixed 52

--- latihan52.js
+++ latihan52.js
@@ -27,8 +27,33 @@ OUTPUT: [
   2. Dilarang menggunakan built in function .filter(), .map(), .forEach(), dan .reduce()
 */
 
-function lotusRealms (clans) {
-
+function lotusRealms(clans) {
+    var result = [];
+    var newArr = [];
+    for (var i = 0; i < clans.length; i++) {
+        newArr.push([
+            clans[i].clan,
+            clans[i].totalHealth
+        ]);
+    }
+    // console.log(newArr);
+    for (var j = 0; j < clans.length; j++) {
+        for (var k = 0; k < newArr.length; k++) {
+            if (clans[j].attack === newArr[k][0]) {
+                newArr[k][1] -= clans[j].totalAttack;
+            }
+        }
+    }
+    // console.log(newArr);
+    for (var l = 0; l < newArr.length; l++) {
+        var newObject = {};
+        if (newArr[l][1] > 0) {
+            newObject.clan = newArr[l][0];
+            newObject.totalHealth = newArr[l][1];
+            result.push(newObject);
+        }
+    }
+    return result;
 }
 
 console.log(lotusRealms([
@@ -45,10 +70,10 @@ console.log(lotusRealms([
   ]
 */
 
-// console.log(lotusRealms([
-//   { clan: 'Wolf', attack: 'Wolf', totalAttack: 300, totalHealth: 600 },
-//   { clan: 'Lotus', attack: 'Wolf', totalAttack: 2000, totalHealth: 3000 }
-// ]))
+console.log(lotusRealms([
+  { clan: 'Wolf', attack: 'Wolf', totalAttack: 300, totalHealth: 600 },
+  { clan: 'Lotus', attack: 'Wolf', totalAttack: 2000, totalHealth: 3000 }
+]))
 /*
   [ { clan: 'Lotus', totalHealth: 3000 } ]
 */
