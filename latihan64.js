0e60aa02be9ad22fd16a25589c1a45693ec3076b
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-18 20:56:12 +0700

    fixed ex 64

--- latihan64.js
+++ latihan64.js
@@ -28,9 +28,36 @@ output: [[0], [0]]
 
 */
 
-function substractEvenOdd (arrNum) {
-
-  
+function substractEvenOdd(arrNum) {
+    var result = [];
+    var genap = [];
+    // console.log(genap[0]);
+    var ganjil = [];
+    for (var i = 0; i < arrNum.length; i++) {
+        if (arrNum[i] % 2 === 0) {
+            genap.push(arrNum[i]);
+        } else {
+            ganjil.push(arrNum[i]);
+        }
+    }
+    var resultGenap = genap[0];
+    var resultGanjil = ganjil[0];
+    for (var j = 1; j < genap.length; j++) {
+        resultGenap -= genap[j];
+    }
+    for (var j = 1; j < ganjil.length; j++) {
+        resultGanjil -= ganjil[j];
+    }
+    if (resultGanjil === undefined && resultGenap === undefined) {
+        result.push([0], [0]);
+    } else if (resultGenap === undefined) {
+        result.push([0], [resultGanjil]);
+    } else if (resultGanjil === undefined) {
+        result.push([resultGenap], [0]);
+    } else {
+        result.push([resultGenap], [resultGanjil]);
+    }
+    return result;
 }
   
 console.log(substractEvenOdd([3, 5, 7, 8])); // [ [8], [-9] ]
