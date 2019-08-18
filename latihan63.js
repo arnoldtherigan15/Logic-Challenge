d01bc05834262d53a713ea55bc94e12647a88900
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-18 20:34:37 +0700

    fixed ex 63

--- latihan63.js
+++ latihan63.js
@@ -40,10 +40,37 @@ output: []
 
 */
 
-function calculate(arrNumber){
+function kali (operand1,operand2) {
+    return operand1*operand2;
+}
+function tambah (operand1,operand2) {
+    return operand1+operand2;
+}
+function kurang (operand1,operand2) {
+    return operand1-operand2;
+}
+function bagi (operand1,operand2) {
+    return operand1/operand2;
+}
 
+function calculate(arrNumber){
+    var result = [];
+    for (var i = 0; i < arrNumber.length; i++) {
+        if (arrNumber[i].operator === '*') {
+            result.push(kali(arrNumber[i].operand1,arrNumber[i].operand2));
+        } else if (arrNumber[i].operator === '+') {
+            result.push(tambah(arrNumber[i].operand1,arrNumber[i].operand2));
+        } else if (arrNumber[i].operator === '-') {
+            result.push(kurang(arrNumber[i].operand1,arrNumber[i].operand2));
+        } else {
+            result.push(bagi(arrNumber[i].operand1,arrNumber[i].operand2));
+        }
+    }
+    return result;
 }
 
 console.log(calculate([{ operand1: 5, operand2: 10, operator: '*' }, { operand1: 3, operand2: 10, operator: '-'}]));
 //[50, -7]
 console.log(calculate([])); //[]
+
+// 5 menit 35 detik
