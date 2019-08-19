7c5545ed440e7356880502ee4d09cf9754f93eda
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-19 23:56:08 +0700

    fixed 70

--- latihan70.js
+++ latihan70.js
@@ -26,22 +26,59 @@ Standard kelulusan adalah minimum 70.
 
 */
 function rapotAsrama(students) {
+    var result = [{
+        asrama: students[0].asrama,
+        gagal: [],
+        lulus: []
+    }];
+    var isFind = false;
+    for (var i = 0; i < students.length; i++) {
+        for (var j = 0; j < result.length; j++) {
+            if (result[j].asrama != students[i].asrama) {
+                var newObject = {
+                    asrama: students[i].asrama,
+                    gagal: [],
+                    lulus: []
+                }
+                if (students[i].nilai >= 70) {
+                    newObject['lulus'].push(students[i].nama);
+                } else {
+                    newObject['gagal'].push(students[i].nama);
+                }
+                isFind = true;
+                
+            }       
+            else {
+                if (students[i].nilai >= 70) {
+                    result[j]['lulus'].push(students[i].nama);
+                    // result.push(newObject);
+                } else {
+                    result[j]['gagal'].push(students[i].nama);
+                    // result.push(newObject);
+                }
+            }
+            if (isFind === true) {
+                result.push(newObject);
+            }
+        }
 
+    }
+    return result;
 }
 
-
+// if (result[j].asrama === students[i].asrama) {
+//     isFind = false;
+// }
 var data = [
   { nama: 'Harry Potter', nilai: 80, asrama: 'Griffindor' },
   { nama: 'Cho Chang', nilai: 60, asrama: 'Ravenclaw' },
   { nama: 'Pansy Parkinson', nilai: 70, asrama: 'Slyterin' },
-  { nama: 'Dean Thomas', nilai: 50, asrama: 'Griffindor' },
-  { nama: 'Ginny Weasley', nilai: 10, asrama: 'Griffindor' },
-  { nama: 'Gregory Goyle', nilai: 10, asrama: 'Slyterin' },
-  { nama: 'Penelope Clearwater', nilai: 10, asrama: 'Ravenclaw' },
-  { nama: 'Tom Marvolo Riddle', nilai: 90, asrama: 'Slyterin' },
+  { nama: 'Dean Thomas', nilai: 50, asrama: 'Griffindor' }
+//   { nama: 'Ginny Weasley', nilai: 10, asrama: 'Griffindor' },
+//   { nama: 'Gregory Goyle', nilai: 10, asrama: 'Slyterin' },
+//   { nama: 'Penelope Clearwater', nilai: 10, asrama: 'Ravenclaw' },
+//   { nama: 'Tom Marvolo Riddle', nilai: 90, asrama: 'Slyterin' }
 ];
-
-
 
 console.log(rapotAsrama(data));
 
