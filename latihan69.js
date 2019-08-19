27a1fccd0824a6444dfd3ac38191c6706c3837f2
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-19 22:08:57 +0700

    fixed 69

--- latihan69.js
+++ latihan69.js
@@ -21,7 +21,56 @@ Function akan membaca data siswa dari input dan mengubahnya menjadi array of obj
 
 
 function dataSiswa(scoreRecord) {
+    var student = '';
+    var students = [];
+    var result = [];
+    if (scoreRecord.length === 0) {
+        return 'tidak ada catatan hasil tes hari ini';
+    }
+    // split untuk tanda koma
+    for (var i = 0; i < scoreRecord.length; i++) {
+        if (scoreRecord[i] === ',') {
+            students.push(student);
+            student = '';
+        } else if (i === scoreRecord.length - 1) {
+            student += scoreRecord[i];
+            students.push(student);
+        } else {
+            student += scoreRecord[i];
+        }
+    }
+    // split untuk tanda -
+    var persons = [[]];
+    var person = '';
+    for (var i = 0; i < students.length; i++) {
+        for (var j = 0; j < students[i].length; j++) {
+            if (students[i][j] === '-') {
+                persons[i].push(person);
+                person = '';
+            } else if (j === students[i].length - 1) {
+                person += students[i][j];
+                persons[i].push(person);
+                person = '';
 
+            } else {
+                person += students[i][j];
+            }
+        }
+        if (i === students.length - 1) {
+            break;
+        }
+        persons.push([]);
+
+    }
+    for (var i = 0; i < persons.length; i++) {
+        var newObject = {
+            id: persons[i][0],
+            name: persons[i][1],
+            score: persons[i][2]
+        }
+        result.push(newObject);
+    }
+    return result;
 }
 
 
@@ -34,7 +83,7 @@ console.log(dataSiswa("001-Radith-88,002-Putra-100,003-Levy-83"))
 ]
  */
 
-// console.log(dataSiswa("001-Radith-5,002-Putra-92,003-Levy-100"))
+console.log(dataSiswa("001-Radith-5,002-Putra-92,003-Levy-100"))
 /* 
 [
   { id: '001', name: 'Radith', score: 5 },
@@ -43,7 +92,7 @@ console.log(dataSiswa("001-Radith-88,002-Putra-100,003-Levy-83"))
 ]
  */
 
-// console.log(dataSiswa("001-Radith-88,002-Putra-70,003-Levy-65,004-Radinka-88,005-Bagas-90"))
+console.log(dataSiswa("001-Radith-88,002-Putra-70,003-Levy-65,004-Radinka-88,005-Bagas-90"))
 /* 
 [
   { id: '001', name: 'Radith', score: 88 },
@@ -54,4 +103,4 @@ console.log(dataSiswa("001-Radith-88,002-Putra-100,003-Levy-83"))
 ]
  */
 
-// console.log(dataSiswa("")) // tidak ada catatan hasil tes hari ini
+console.log(dataSiswa("")) // tidak ada catatan hasil tes hari ini
