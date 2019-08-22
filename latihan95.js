4f8607d8fa656672f3e2b1b1f57ee24d5c93be3c
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-22 17:19:32 +0700

    fixed 95

--- latihan95.js
+++ latihan95.js
@@ -1,2 +1,12 @@
 // INPUT 'Hellow world!'
 // OUTPUT !dlrow olleH
+
+function balikKata (str){
+    // console.log(str.slice(0,-1));
+    if (str.length === 1) {
+        return str[str.length-1];
+    } else {
+        return str[str.length-1] + balikKata(str.slice(0,-1));
+    }
+}
+console.log(balikKata('hello world'));
