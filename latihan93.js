03377d0579fd84fd0a3d7f78e3c0c5ea012edeab
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-22 16:26:51 +0700

    add ex 93

--- latihan93.js
+++ latihan93.js
@@ -1,10 +1,33 @@
-// 3)
 // function cariPelaku(str) {
-//   //REKURSIF
+//   var count = 0;
+//   for (var i = 0; i < str.length; i++) {
+//       if (str[i] === 'a') {
+//           if(str[i+1] === 'b') {
+//               if (str[i+2] === 'c') {
+//                   count++;
+//               }
+//           }
+//       }
+//   }
+//   return count;
 // }
-// // TEST CASES
-// console.log(cariPelaku('mabcvabc')); // 2
-// console.log(cariPelaku('abcdabdc')); // 1
-// console.log(cariPelaku('bcabcac')); // 1
-// console.log(cariPelaku('abcabcabc')); // 3
-// console.log(cariPelaku('babcbacabc')); // 2
+function cariPelaku(str) {
+    var count = 0;
+    if (str.length === 0) {
+        return count;
+    } else {
+        if(str[0] === 'a' && str[1] === 'b' && str[2] === 'c') {
+            count++;
+            return count + cariPelaku(str.slice(1));
+        } else {
+            return cariPelaku(str.slice(1));
+        }
+    }
+}
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
+
