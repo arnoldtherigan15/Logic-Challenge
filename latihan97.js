ab2a2f494fc00c7b656280335aba7c03038b399e
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-23 17:41:05 +0700

    add ex 99

--- latihan97.js
+++ latihan97.js
@@ -1,26 +1,46 @@
+// function cekPrima (num) {
+//     var counter = 0;
+//     for (var i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             counter++;
+//         }
+//     }
+//     if (num === 1) {
+//         return false;
+//     } else if (counter === 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 function cekPrima (num) {
     var counter = 0;
-    for (var i = 2; i < num; i++) {
-        if (num % i === 0) {
-            counter++;
-        }
-    }
-    if (num === 1) {
-        return false;
-    } else if (counter === 0) {
-        return true;
-    } else {
-        return false;
-    }
-}
-console.log(cekPrima(3));
-function cekPrima (num) {
-    if (num === 2) {
-        return 0;
+    if (num === 0) {
+        return counter;
     } else {
         if (num % num-1 === 0) {
-            return 0 + cekPrima(num-1);
+            counter++;
+            return counter + cekPrima(num-1);
+        } 
+        if (counter === 0) {
+            return true;
+        } else {
+            return false;
         }
     }
+ 
+    // if (num === 1) {
+    //     return false;
+    // } 
 }
+console.log(cekPrima(4));
+// function cekPrima (num) {
+//     if (num === 2) {
+//         return 0;
+//     } else {
+//         if (num % num-1 === 0) {
+//             return 0 + cekPrima(num-1);
+//         }
+//     }
+// }
 // console.log(cekPrima(3));
--- latihan98.js
+++ latihan98.js
@@ -12,14 +12,33 @@ RULES
 - Dilarang mengubah tipe data parameter
 
 */
-function angkaTerbesar(array) {
-    // you can only write your code here!
-  
-  }
-  
-  // TEST CASES
-  console.log(angkaTerbesar([2, 3, 7, 6, 5])) // 7
-  console.log(angkaTerbesar([9, 3, 7, 4, 1])) // 9
-  console.log(angkaTerbesar([2, 1, 7, 2, 8])) // 7
-  console.log(angkaTerbesar([])) // -1
+// function angkaTerbesar(array) {
+//     if (array.length === 0) {
+//         return -1;
+//     }
+//    for (var i = 0; i < array.length; i++) {
+//        for (var j = i+1; j <array.length; j++) {
+//             if (array[i] > array[j]) {
+//                 [array[i],array[j]] = [array[j],array[i]];
+//             }
+//        }
+//    }
+//    return array[array.length-1];
+// }
+function angkaTerbesar(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    } 
+        if (arr[0] > arr[1]) {
+            arr[1] = arr[0]
+        } 
+            return angkaTerbesar(arr.splice(1))
+        
+    }
+}
+// TEST CASES
+console.log(angkaTerbesar([2, 3, 7, 6, 5])) // 7
+// console.log(angkaTerbesar([9, 3, 7, 4, 1])) // 9
+// console.log(angkaTerbesar([2, 1, 7, 2, 8])) // 8
+// console.log(angkaTerbesar([])) // -1
   
