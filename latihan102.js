dc476a506846fa3172bb7a06163a5be0c223419f
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-23 21:13:50 +0700

    fix 102

--- latihan102.js
+++ latihan102.js
@@ -23,12 +23,55 @@ output: ['you', 'it!']
 */
 
 function shortestWords(words) {
-
+    var result = [];
+    var temp = '';
+    for (var i = 0; i < words.length; i++) {
+        if (words[i] === ' ') {
+            result.push(temp);
+            temp = '';
+        } else {
+            temp += words[i]
+        }
+        if (i === words.length - 1) {
+            result.push(temp);
+        }
+    }
+    for (var i = 0; i < result.length; i++) {
+        for (var j = i + 1; j < result.length; j++) {
+            if (result[i].length > result[j].length) {
+                [
+                    result[i], result[j]
+                ] = [
+                    result[j], result[i]
+                ];
+            }
+        }
+    }
+    var kataTerpendek = [];
+    for (var i = 0; i < result.length; i++) {
+        if (result[i].length === result[0].length) {
+            kataTerpendek.push(result[i]);
+        }
+    }
+    var resultFilter = [];
+    resultFilter.push(kataTerpendek[0]);
+    for (var i = 0; i < kataTerpendek.length; i++) {
+        var isDuplikat = false;
+        for (var j = 0; j < resultFilter.length; j++) {
+            if (resultFilter[j].toLowerCase() === kataTerpendek[i].toLowerCase()) {
+                isDuplikat = true;
+            }
+        }
+        if (isDuplikat === false) {
+            resultFilter.push(kataTerpendek[i]);
+        }
+    }
+    return resultFilter;
 }
 
 // TEST CASE
 
 console.log(shortestWords('Do you want to become a great coder ?')); // ['a', '?']
-console.log(shortestWords('You dont know what you have until you lose it!')); // ['you', 'it!'] \\ kalo keluarnya ["You"] boleh ga?
+console.log(shortestWords('You dont know what you have until you lose it!')); // ['you', 'it!'] 
 console.log(shortestWords('I am diligent')); // ['I']
 console.log(shortestWords('Apapun hasilnya, kita sudah berusaha, serahkan saja')); // ['kita', 'saja']
