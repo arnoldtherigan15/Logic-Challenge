956b9056019b2019acd29ff1ced48ea54205f737
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-27 13:13:17 +0700

    update

--- latihan139.js
+++ latihan139.js
@@ -29,9 +29,55 @@ function economyChangeSummary (tradeActivity) {
       { name: 'Jeff Bezos', deposit: 100000, owner: 'Amazon' },
       { name: 'Jack Ma', deposit: 90000, owner: 'Alibaba' },
       { name: 'Larry Page', deposit: 95000, owner: 'Google' }
-    ]
-    // YOUR CODE GOES HERE
-
+    ];
+    var newArr = [];
+    for (var i = 0; i < tradeActivity.length; i++) {
+        newArr.push([]);
+        for (var j = 0; j < tradeActivity[i].length; j++) {
+            var temp = '';
+            newArr[i].push([]);
+            for (var k = 0; k < tradeActivity[i][j].length; k++) {
+               
+                if (tradeActivity[i][j][k] === '+' || tradeActivity[i][j][k] === '%' || tradeActivity[i][j][k] === '-') {
+                    newArr[i][j].push(temp);
+                    temp = '';
+                }
+                else {
+                    temp += tradeActivity[i][j][k];
+                }
+                if (tradeActivity[i][j][k] === '+') {
+                    newArr[i][j].push('+');
+                } else if (tradeActivity[i][j][k] === '-') {
+                    newArr[i][j].push('-');
+                }
+            }
+        }
+    }
+    // console.log(newArr);
+    var result = [];
+    for (var i = 0; i < newArr.length; i++) {
+        for (var j = 0; j < newArr[i].length; j++) {
+          
+          for (var k = 0; k < bankAccount.length; k++) {
+            if (bankAccount[k]['name'] === newArr[i][j][0]) {
+              if(newArr[i][j][1] === '+') {
+                bankAccount[k]['deposit'] += ((bankAccount[k]['deposit'])*Number(newArr[i][j][2]))/100;
+                result.push({name: newArr[i][j][0], deposit: bankAccount[k]['deposit'], owner: bankAccount[k]['owner']})
+                
+              } else {
+                bankAccount[k]['deposit'] -= ((bankAccount[k]['deposit'])*Number(newArr[i][j][2]))/100;
+                result.push({name: newArr[i][j][0], deposit: bankAccount[k]['deposit'], owner: bankAccount[k]['owner']})
+              }
+              
+            }
+          }
+          
+          
+        }
+    }
+    return result;
+    
+    
 }
 console.log(economyChangeSummary([
   ['Jeff Bezos+5%', 'Larry Page+10%', 'Jeff Bezos-3%'],
@@ -52,9 +98,9 @@ console.log(economyChangeSummary([
   { name: 'Jeff Bezos', deposit: 106698.06, owner: 'Amazon' } ]
 */
 
-console.log(economyChangeSummary([
-  ['Jeff Bezos-10%']
-]))
+// console.log(economyChangeSummary([
+//   ['Jeff Bezos-10%']
+// ]))
 /*
       [ { name: 'Jeff Bezos', deposit: 90000, owner: 'Amazon' } ]
     */
