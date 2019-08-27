671b1dd0a9d8892931a3ed2f3c4eee6c1383f424
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-27 11:56:43 +0700

    update

--- latihan139.js
+++ latihan139.js
@@ -1,22 +1,60 @@
-function cariMean(arr,length) {
-    if (arr.length === 0) {
-        return 0
-    } else {
-        console.log(arr);
-        if (arr.length === 1) {
-            console.log('---');
-            
-            return (arr[0] + cariMean(arr.slice(1)))/length;
-        }
-        
-        return arr[0] + cariMean(arr.slice(1));
-    }
+/*
+=========
+HACKATHON
+=========
+[INSTRUCTION]
+Buatlah suatu aplikasi untuk membuat catatan ekonomi.
+[EXAMPLE]
+"bank account sudah disediakan"
+input: [['Jeff Bezos+5%', 'Larry Page+10%', 'Jeff Bezos-3%'], ['Larry Page+2%', 'Larry Page-1%']]
+process:
+  bank account => deposit atas nama Jeff Bezos ditambah 5%, menjadi 105000
+  bank account => deposit atas nama Larry Page ditambah 10%, mejadi  104500
+  bank account => deposit atas nama Jeff Bezos dikurangi 3%, mejadi 101850
+  ...dst
+output:
+  [
+    { name: 'Jeff Bezos', deposit: 105000, owner: 'Amazon' },
+    { name: 'Larry Page', deposit: 104500, owner: 'Google' },
+    { name: 'Jeff Bezos', deposit: 101850, owner: 'Amazon' },
+    { name: 'Larry Page', deposit: 106590, owner: 'Google' },
+    { name: 'Larry Page', deposit: 105524.1, owner: 'Google' }
+  ]
+[RULES]
+- Dilarang menggunakan .indexOf(), .split(), .filter(), .map(), dan .slice()
+*/
+
+function economyChangeSummary (tradeActivity) {
+    var bankAccount = [
+      { name: 'Jeff Bezos', deposit: 100000, owner: 'Amazon' },
+      { name: 'Jack Ma', deposit: 90000, owner: 'Alibaba' },
+      { name: 'Larry Page', deposit: 95000, owner: 'Google' }
+    ]
+    // YOUR CODE GOES HERE
 
 }
+console.log(economyChangeSummary([
+  ['Jeff Bezos+5%', 'Larry Page+10%', 'Jeff Bezos-3%'],
+  ['Larry Page+2%', 'Larry Page-1%'],
+  ['Jack Ma+4%'],
+  ['Larry Page-8%', 'Jack Ma+20%', 'Jeff Bezos-3%', 'Jeff Bezos+8%']
+]))
+/*
+  [ { name: 'Jeff Bezos', deposit: 105000, owner: 'Amazon' },
+  { name: 'Larry Page', deposit: 104500, owner: 'Google' },
+  { name: 'Jeff Bezos', deposit: 101850, owner: 'Amazon' },
+  { name: 'Larry Page', deposit: 106590, owner: 'Google' },
+  { name: 'Larry Page', deposit: 105524.1, owner: 'Google' },
+  { name: 'Jack Ma', deposit: 93600, owner: 'Alibaba' },
+  { name: 'Larry Page', deposit: 97082.172, owner: 'Google' },
+  { name: 'Jack Ma', deposit: 112320, owner: 'Alibaba' },
+  { name: 'Jeff Bezos', deposit: 98794.5, owner: 'Amazon' },
+  { name: 'Jeff Bezos', deposit: 106698.06, owner: 'Amazon' } ]
+*/
 
-// TEST CASES
-console.log(cariMean([1, 2, 3, 4, 5],5)); // 3
-// console.log(cariMean([3, 5, 7, 5, 3])); // 5
-// console.log(cariMean([6, 5, 4, 7, 3])); // 5
-// console.log(cariMean([1, 3, 3])); // 2
-// console.log(cariMean([7, 7, 7, 7, 7])); // 7
+console.log(economyChangeSummary([
+  ['Jeff Bezos-10%']
+]))
+/*
+      [ { name: 'Jeff Bezos', deposit: 90000, owner: 'Amazon' } ]
+    */
