68754d343dc7f61443c992e5c643e7d79c2174f4
arnold_therigan <arnoldtherigan15@gmail.com>
2019-08-27 10:35:42 +0700

    update

--- latihan132.js
+++ latihan132.js
@@ -13,11 +13,24 @@ RULES
 
 */
 function angkaTerbesar(array) {
+    if (array.length === 0) {
+        return -1;
+    }
+    if (array.length === 1) {
+        return array[0];
+    } else {
+        if (array[1] > array[0]) {
+            array[0] = array[1];
+        } else {
+            array[1] = array[0];
+        }
+        return angkaTerbesar(array.slice(1));
+    }
 
 }
 
 // TEST CASES
 console.log(angkaTerbesar([2, 3, 7, 6, 5])) // 7
 console.log(angkaTerbesar([9, 3, 7, 4, 1])) // 9
-console.log(angkaTerbesar([2, 1, 7, 2, 8])) // 7
+console.log(angkaTerbesar([2, 1, 7, 2, 8])) // 8
 console.log(angkaTerbesar([])) // -1
